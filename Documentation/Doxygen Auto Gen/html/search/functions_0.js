98691bba4c4dc0893e5a9bcddc87f0dae25d5cdd
Dylan Barnes <dylan.barnes@emerson.com>
2016-08-30 11:27:36 -0500

    Updated README and generated new HTML documentation, while depricating other documentation.

--- Documentation/Doxygen Auto Gen/html/search/functions_0.js
+++ Documentation/Doxygen Auto Gen/html/search/functions_0.js
@@ -1,4 +1,8 @@
 var searchData=
 [
-  ['dispose',['Dispose',['../class_d_c_s___converter_1_1_data_view.html#acb9771c2a820e1a886dc0764672fd32a',1,'DCS_Converter.DataView.Dispose()'],['../class_d_c_s___converter_1_1_form1.html#acb4352e23589b1f35bffe3559e0193af',1,'DCS_Converter.Form1.Dispose()']]]
+  ['objtodatatable',['ObjToDataTable',['../class_dcs_converter_1_1_dcs_all.html#ada643939455a23ca019d51baa42d4b53',1,'DcsConverter::DcsAll']]],
+  ['outputcsv',['OutputCsv',['../class_dcs_converter_1_1_dcs_csv.html#ac98bc36c7aa650d232c3a20fa5a46c1e',1,'DcsConverter::DcsCsv']]],
+  ['outputfile',['OutputFile',['../class_dcs_converter_1_1_dcs_all.html#af816ee6421b26f3baaa2336985b0d5c3',1,'DcsConverter::DcsAll']]],
+  ['outputjson',['OutputJson',['../class_dcs_converter_1_1_dcs_json.html#a4f3969afe3d840a693204ce80a05e8e8',1,'DcsConverter::DcsJson']]],
+  ['outputxml',['OutputXml',['../class_dcs_converter_1_1_dcs_xml.html#acbe2527691adbde5e546425dfa043c7a',1,'DcsConverter::DcsXml']]]
 ];
--- Documentation/Doxygen Auto Gen/html/search/functions_1.js
+++ Documentation/Doxygen Auto Gen/html/search/functions_1.js
@@ -1,5 +1,11 @@
 var searchData=
 [
-  ['filebrowser',['fileBrowser',['../class_d_c_s___converter_1_1_form1.html#a44b7988c9073472efe9ea2db3e83d20d',1,'DCS_Converter::Form1']]],
-  ['form1',['Form1',['../class_d_c_s___converter_1_1_form1.html#afae5f6a40eb8dbc0e481e8aa6a429d46',1,'DCS_Converter::Form1']]]
+  ['parsecsv',['ParseCsv',['../class_dcs_converter_1_1_dcs_csv.html#a1013aaf9f7ae96e559eb26b8d3839236',1,'DcsConverter::DcsCsv']]],
+  ['parsecsv_5fasjsonarray',['parseCSV_AsJsonArray',['../class_dcs_converter_1_1_dcs_csv.html#a9f164341fb02e6f19a83a7e33b04aba6',1,'DcsConverter::DcsCsv']]],
+  ['parsecsvdata',['ParseCsvData',['../class_dcs_converter_1_1_dcs_csv.html#ac3ba6f2ee9b5f1ee562cfa844949808f',1,'DcsConverter::DcsCsv']]],
+  ['parsefile',['ParseFile',['../class_dcs_converter_1_1_dcs_all.html#aba61690745611459cf96edb413e52e84',1,'DcsConverter::DcsAll']]],
+  ['parsejson',['ParseJson',['../class_dcs_converter_1_1_dcs_json.html#abbf379cca8327825f7cdcece1ed9737d',1,'DcsConverter::DcsJson']]],
+  ['parsejsondata',['ParseJsonData',['../class_dcs_converter_1_1_dcs_json.html#ac30160f11c1265a2b48e387546d98c06',1,'DcsConverter::DcsJson']]],
+  ['parsexml',['ParseXml',['../class_dcs_converter_1_1_dcs_xml.html#adec4d94b9047770837856cc299184ac0',1,'DcsConverter::DcsXml']]],
+  ['parsexmldata',['ParseXmlData',['../class_dcs_converter_1_1_dcs_xml.html#a0973a5d59716efa31b233db79b397534',1,'DcsConverter::DcsXml']]]
 ];
